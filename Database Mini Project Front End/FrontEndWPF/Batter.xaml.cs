e51b270f98abb006c46cd22176a7231e1796cb73
Yosef Schoen <josephaschoen@gmail.com>
2021-06-10 12:33:38 +0300

    Add files via upload

---
+++ Database Mini Project Front End/FrontEndWPF/Batter.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+using System.Data;
+using System.Configuration;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
+
+namespace FrontEndWPF
+{
+    /// <summary>
+    /// Interaction logic for Batter.xaml
+    /// </summary>
+    public partial class Batter : Window
+    {
+        OracleConnection myConnection = null;
+        public Batter()
+        {
+            this.setConnection();
+            InitializeComponent();
+        }
+
+        private void setConnection()
+        {
+            String connectionString = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
+            myConnection = new OracleConnection(connectionString);
+
+            try
+            {
+                myConnection.Open();
+            }
+            catch (Exception notOpen)
+            {
+
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            String myQuery = "SELECT * FROM Batter";
+            this.updateGrid(myQuery);
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            myConnection.Close();
+        }
+
+        private void updateGrid(String myQuery)
+        {
+            OracleCommand cmd = myConnection.CreateCommand();
+            cmd.CommandText = myQuery;
+            cmd.CommandType = CommandType.Text;
+            OracleDataReader dr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            BatterDataGrid.ItemsSource = dt.DefaultView;
+            dr.Close();
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            String batterID = SearchBatter.Text;
+            String myQuery = "BEGIN BATTERUI.FINDBATTER('" + batterID + "'); END;";
+            updateGrid(myQuery);
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            new Main().Show();
+            this.Close();
+        }
+    }
+}
---
+++ Database Mini Project Front End/FrontEndWPF/Main.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FrontEndWPF
+{
+    /// <summary>
+    /// Interaction logic for Main.xaml
+    /// </summary>
+    public partial class Main : Window
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Team_Click(object sender, RoutedEventArgs e)
+        {
+            new Team().Show();
+            this.Close();
+        }
+
+        private void Player_Click(object sender, RoutedEventArgs e)
+        {
+            new Player().Show();
+            this.Close();
+        }
+
+
+        private void Pitcher_Click(object sender, RoutedEventArgs e)
+        {
+            new Pitcher().Show();
+            this.Close();
+        }
+
+        private void Batter_Click(object sender, RoutedEventArgs e)
+        {
+            new Batter().Show();
+            this.Close();
+        }
+
+    }
+}
